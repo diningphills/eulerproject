6728f37e935fdc4361893daea53ca871458c1eda
Kim Kangsan <wongrikera@nate.com>
2015-08-27 22:57:01 +0900

    Solved 22 by sort()

---
+++ problems/22/kks227.py
@@ -0,0 +1,8 @@
+﻿f = open('names.txt', 'rt')
+names = f.readlines()[0].replace('"', '').split(',')
+names.sort()
+result = 0
+for i in range(len(names)):
+	for c in names[i]:
+		result += (ord(c)-ord('A')+1)*(i+1)
+print(result)
