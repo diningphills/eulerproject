8639fa0e42838008855d156723924abac8e9867d
Kim Kangsan <wongrikera@nate.com>
2015-08-27 23:10:35 +0900

    Solved 42 by generator

---
+++ problems/42/kks227.py
@@ -0,0 +1,11 @@
+﻿f = open('words.txt', 'rt')
+words = f.readlines()[0].replace('"', '').split(',')
+tri = [int(i*(i+1)/2) for i in range(50)]
+count = 0
+for w in words:
+	temp = 0
+	for c in w:
+		temp += ord(c)-ord('A')+1
+	if temp in tri:
+		count += 1
+print(count)
